ac0dbd2298ad3b84f948cbc75ee0d1c8b44c1bbf
Lkss <lucasdesiderio000@gmail.com>
2025-08-14 13:14:30 -0300

    Se agregaron las llamadas al archivo principal

--- TP 2 - Programación Estructurada/TP_2_Programacion_Estructurada/src/tp_2_programacion_estructurada/TP_2_Programacion_Estructurada.java
+++ TP 2 - Programación Estructurada/TP_2_Programacion_Estructurada/src/tp_2_programacion_estructurada/TP_2_Programacion_Estructurada.java
@@ -14,7 +14,19 @@ public class TP_2_Programacion_Estructurada {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Ejercicio_1.main(args);
+        Ejercicio_2.main(args);
+        Ejercicio_3.main(args);
+        Ejercicio_4.main(args);
+        Ejercicio_5.main(args);
+        Ejercicio_6.main(args);
+        Ejercicio_7.main(args);
+        Ejercicio_8.main(args);
+        Ejercicio_9.main(args);
+        Ejercicio_10.main(args);
+        Ejercicio_11.main(args);
+        Ejercicio_12.main(args);
+        Ejercicio_13.main(args);
     }
     
 }
